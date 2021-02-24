e6d8f44de09ada38c44b14afc55bc9be6e27fd07
Durran Jordan <durran@gmail.com>
2021-02-24 11:33:18 +0100

    Initial commit

---
+++ Roubaix/RoubaixApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoubaixApp.swift
+//  Roubaix
+//
+//  Created by Durran Jordan on 24.02.21.
+//
+
+import SwiftUI
+
+@main
+struct RoubaixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
